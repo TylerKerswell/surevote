779abfebfd327a2e804266c62fe50551b649b259
Tyler Kerswell <tylerkerswell@gmail.com>
2024-10-05 23:16:33 -0700

    changed path

--- backend/endpoints/endpoints.js
+++ backend/endpoints/endpoints.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 function initEndpoints(express) {
     // Temp test
     express.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../../frontend/test/home.html'));
+        res.sendFile(path.join(__dirname, '../public/home.html'));
     });
     // Frontend app makes request to our /getLocationStatus endpoint, and we request from given API
 
@@ -17,55 +17,55 @@ function initEndpoints(express) {
     express.get("/admin/getvoters/", getvoters);
 
 
-    express.get("/getLocationStatus/:lat/:lon", (req, res) => {
-        let lat = req.params.lat;
-        let lon = req.params.lon;
-        if (!lat || !lon) {
-            res.status(400).send("bad request, missing lat and/or lon")
-            return;
-        }
+    // express.get("/getLocationStatus/:lat/:lon", (req, res) => {
+    //     let lat = req.params.lat;
+    //     let lon = req.params.lon;
+    //     if (!lat || !lon) {
+    //         res.status(400).send("bad request, missing lat and/or lon")
+    //         return;
+    //     }
 
-        fetch('https://pplx.azurewebsites.net/api/rapid/v0/location-verification/verify', {
-            method: "POST",
-            body: JSON.stringify(
-                {
-                    "device": {
-                        "phoneNumber": "14372307313"
-                    },
-                    "area": {
-                        "type": "Circle",
-                        "location": { "latitude": lat, "longitude": lon },
-                        "accuracy": 1,
-                    }
-                }
-            ),
-            headers: {
-                "Authorization": "Bearer 166b4a",
-                "Content-Type": "application/json",
-                "Cache-Control": "no-cache",
-                "accept": "application/json"
-            }
-        }).then((ApiRes) => {
-            if (ApiRes.ok) {
-                return ApiRes.json();
-            } else {
-                console.log("API response was not ok:", ApiRes.status);
-                throw new Error("API response was not ok");
-            }
-        }).then((json) => {
-            if (json.verificationResult == null) {
-                console.log("Malformed request body");
-                res.status(400).send("Malformed request body from API");
-            } else if (json.verificationResult) {
-                res.status(200).send(`yes, this number is at ${lat}, ${lon}`);
-            } else if (!json.verificationResult) {
-                res.status(200).send(`nope, this number is NOT at ${lat}, ${lon}`);
-            }
-        }).catch((error) => {
-            console.error("Fetch error:", error.message);
-            res.status(500).send("Internal server error: " + error.message);
-        });
-    });
+    //     fetch('https://pplx.azurewebsites.net/api/rapid/v0/location-verification/verify', {
+    //         method: "POST",
+    //         body: JSON.stringify(
+    //             {
+    //                 "device": {
+    //                     "phoneNumber": "14372307313"
+    //                 },
+    //                 "area": {
+    //                     "type": "Circle",
+    //                     "location": { "latitude": lat, "longitude": lon },
+    //                     "accuracy": 1,
+    //                 }
+    //             }
+    //         ),
+    //         headers: {
+    //             "Authorization": "Bearer 166b4a",
+    //             "Content-Type": "application/json",
+    //             "Cache-Control": "no-cache",
+    //             "accept": "application/json"
+    //         }
+    //     }).then((ApiRes) => {
+    //         if (ApiRes.ok) {
+    //             return ApiRes.json();
+    //         } else {
+    //             console.log("API response was not ok:", ApiRes.status);
+    //             throw new Error("API response was not ok");
+    //         }
+    //     }).then((json) => {
+    //         if (json.verificationResult == null) {
+    //             console.log("Malformed request body");
+    //             res.status(400).send("Malformed request body from API");
+    //         } else if (json.verificationResult) {
+    //             res.status(200).send(`yes, this number is at ${lat}, ${lon}`);
+    //         } else if (!json.verificationResult) {
+    //             res.status(200).send(`nope, this number is NOT at ${lat}, ${lon}`);
+    //         }
+    //     }).catch((error) => {
+    //         console.error("Fetch error:", error.message);
+    //         res.status(500).send("Internal server error: " + error.message);
+    //     });
+    // });
 }
 
 // Function for registration endpoint with number verification only
