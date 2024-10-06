72abb91eaa8a99fb69f8825af0e12e463e01d97a
TylerKerswell <tylerkerswell@gmail.com>
2024-10-06 05:56:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const initEndpoints = require('./endpoints/endpoints.js');
+const path = require('path');
+
+
+class Server {
+    constructor(port) {
+        this.port = port;
+        this.app = express();
+        this.initMiddlewares();
+        
+        initEndpoints(this.app);
+    }
+
+    initMiddlewares() {
+        this.app.use(express.json());
+        this.app.use('/assets', express.static(path.join(__dirname, '../frontend/test')));
+        this.app.use(express.raw({ type: "application/*", limit: "10mb" }));
+        this.app.use(cors());
+    }
+
+
+    start() {
+        this.server = this.app.listen(this.port, () => {
+            console.log(`Server started on http://localhost:${this.port}`);
+        });
+    }
+
+    stop() {
+        this.server.close(() => {
+            console.log("Stopped");
+        });
+    }
+
+
+}
+
+module.exports = Server;
---
+++ frontend/test/scripts/reg.js
@@ -0,0 +1,6 @@
+var submit = document.getElementById("submit");
+if(submit) {
+submit.addEventListener("click", function (e) {
+// Add your code here
+});
+}
