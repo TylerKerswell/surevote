9e1c63fcc61e1c28bb706ced05bb0d5aba3c71ba
mhuang306 <mhuang306@gmail.com>
2024-10-06 12:33:23 -0700

    dfd

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,7 +1,7 @@
 document.querySelector(".submit").addEventListener("click", async function (e) {
 
     let msg = document.getElementById("msg").value;
-
+    console.log(msg);
 
     const response = await fetch('https://surevote.vercel.app/admin/startvote', {
         method: 'POST',
@@ -9,16 +9,19 @@ document.querySelector(".submit").addEventListener("click", async function (e) {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer 166b4a', // Replace with your actual token if necessary
         },
-        body: {
+        body: JSON.stringify({
             "sim_swap_date": "2023-10-06T01:43:03.171Z", 
             "lat": "49.266910", 
             "lon": "-123.247415", 
             "accuracy": "100", 
             "ballot_message": `${msg}`,
-        }
+        })
     });
 
 
-
+    const result = await response.json(); 
+    console.log(result);
+    
+    
 
 });
