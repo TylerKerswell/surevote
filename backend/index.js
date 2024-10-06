ce44cc2ae57157dbfc9feb5f7cd85e4fe466fde5
Tyler Kerswell <tylerkerswell@gmail.com>
2024-10-05 23:14:24 -0700

    config changes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,23 @@
-const Server = require('./server.js'); 
+const express = require('express');
+const cors = require('cors');
+const initEndpoints = require('./endpoints/endpoints.js');
+const path = require('path');
+const serverless = require('serverless-http');
 
-const PORT = 3000;
+// Initialize Express app
+const app = express();
 
-let server = new Server(PORT);
+// Initialize middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-server.start();
+// Serve static files
+app.use('/assets', express.static(path.join(__dirname, '../public')));
 
+// Initialize your endpoints
+initEndpoints(app);
+
+// Export the app and handler
+module.exports = app;
+module.exports.handler = serverless(app);
